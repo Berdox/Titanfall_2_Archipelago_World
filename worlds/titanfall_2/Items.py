0c08939eb57ada92f0ae51e68150aa7ffd8bb80b
Berdox <ckdhodge@gmail.com>
2026-05-03 21:32:32 -0700

    added files

---
+++ worlds/titanfall_2/Items.py
@@ -0,0 +1,66 @@
+
+
+
+
+'''
+Helments list (46):
+
+Gauntlet 1 
+
+BT-7274 1
+BT-7274 2
+
+Blood and Rust 1
+Blood and Rust 2
+Blood and Rust 3
+Blood and Rust 4
+Blood and Rust 5
+Blood and Rust 6
+
+Into the Abyss (1) 1
+Into the Abyss (1) 2
+Into the Abyss (1) 3
+Into the Abyss (1) 4
+
+Into the Abyss (2) 1
+Into the Abyss (2) 2
+Into the Abyss (2) 3
+
+Into the Abyss (3) 1
+Into the Abyss (3) 2
+
+Effect and Cause (1) 1
+Effect and Cause (1) 2
+
+Effect and Cause (2) 1
+Effect and Cause (2) 2
+Effect and Cause (2) 3
+Effect and Cause (2) 4
+Effect and Cause (2) 5
+Effect and Cause (2) 6
+
+The Beacon (1) 1
+The Beacon (1) 2
+
+The Beacon (2) 1
+The Beacon (2) 2
+The Beacon (2) 3
+The Beacon (2) 4
+The Beacon (2) 5
+The Beacon (2) 6
+The Beacon (2) 7
+The Beacon (2) 8
+The Beacon (2) 9
+
+Trail by Fire 1
+Trail by Fire 2
+Trail by Fire 3
+
+The Ark 1
+The Ark 2
+The Ark 3
+
+The Fold Weapon 1
+The Fold Weapon 2
+The Fold Weapon 3
+'''
---
+++ worlds/titanfall_2/Locations.py
@@ -0,0 +1,39 @@
+
+
+
+'''
+List of Missions:
+
+The Pilot's Gauntlet
+
+BT-7274
+
+Blood and Rust
+
+Into the Abyss
+
+Effect and Cause
+
+The Beacon
+
+Trail by Fire
+
+The Ark
+
+The Fold Weapon
+'''
+
+'''
+Bosses:
+
+Kane
+
+Ash
+
+Richter
+
+Viper
+
+Slone
+
+'''
